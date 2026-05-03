3c2713445ba7ce765a2988077db2f362e78aa48b
Nem0tive <mae.klinkenberg@icloud.com>
2026-05-03 12:03:18 +0200

    comments

--- ns_job.sh
+++ ns_job.sh
@@ -7,19 +7,19 @@
 #SBATCH --output=resultats/resultats_%j.txt
 
 
-# --- Initialisation de l'environnement ---
-echo "Initialisation de l'environnement"
+# Environment Initialization
+echo "Initializing environment..."
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate cvu-mesh
 
-# --- Lancement de l'extraction avec l'IA ---
-echo "Lancement de ns-process-data avec hloc..."
+# Launching AI-based extraction
+echo "Launching ns-process-data with hloc"
 ns-process-data video --data $(pwd)/data/S1_V2.mp4 --output-dir $(pwd)/data/project-S1_V2 --sfm-tool hloc
 
 for f in $(pwd)/data/S[2-9]*.mp4; do 
     base=$(basename "$f" .mp4)
-    echo "Lancement de ns-process-data avec hloc de $base"
+    echo "Launching ns-process-data with hloc for $base"
     ns-process-data video --data "$f" --output-dir "$(pwd)/data/project-$base" --sfm-tool hloc
 done
 
-echo "FIN DU TRAITEMENT"
+echo "Processing finished"
