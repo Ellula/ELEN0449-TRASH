d383c9546b4a75f2ab1af6b9ccaab16b4b6603b6
Nem0tive <mae.klinkenberg@icloud.com>
2026-05-05 09:55:52 +0200

    script for all videos

--- ns_job.sh
+++ ns_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=colmap_hloc
-#SBATCH --time=24:00:00            
+#SBATCH --time=30:00:00            
 #SBATCH --ntasks=1
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:1
@@ -13,10 +13,7 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate cvu-mesh
 
 # Launching AI-based extraction
-echo "Launching ns-process-data with hloc"
-ns-process-data video --data $(pwd)/data/S1_V2.mp4 --output-dir $(pwd)/data/project-S1_V2 --sfm-tool hloc
-
-for f in $(pwd)/data/S[2-9]*.mp4; do 
+for f in $(pwd)/data/S[1-9]*.mp4; do 
     base=$(basename "$f" .mp4)
     echo "Launching ns-process-data with hloc for $base"
     ns-process-data video --data "$f" --output-dir "$(pwd)/data/project-$base" --sfm-tool hloc
